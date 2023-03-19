f9e60110895acf425a924e1207843336d0e94c6c
Frederik <frederikblockchain@gmail.com>
2023-03-19 22:59:36 +0100

    started on search. Missing layout of search

--- androidApp/src/main/java/com/example/myapplication/android/Components/IngredientListView.kt
+++ androidApp/src/main/java/com/example/myapplication/android/Components/IngredientListView.kt
@@ -1,22 +1,44 @@
 package com.example.myapplication.android.Components
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectVerticalDragGestures
+import androidx.compose.foundation.gestures.scrollBy
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import com.example.myapplication.Models.Ingredient
 import com.example.myapplication.android.SQLite.DBHandler
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
-fun IngredientListView(ingredients: List<Ingredient>){
-    Column() {
-        ingredients.forEach {
-            IngredientCard(ingredient = it)
+fun IngredientListView(ingredients: List<Ingredient>) {
+    val scrollState = rememberScrollState()
+    val coroutine = rememberCoroutineScope()
+    Box(modifier = Modifier.height(430.dp)) {
+        LazyColumn(modifier = Modifier
+            .scrollable(
+                scrollState,
+                orientation = Orientation.Vertical,
+                enabled = true
+            )
+            .pointerInput(Unit) {
+                detectVerticalDragGestures { _, dragAmount ->
+                    coroutine.launch { scrollState.scrollBy(dragAmount) }
+                }
+            }) {
+            items(ingredients) {
+                IngredientCard(ingredient = it)
+            }
         }
     }
-}
-
-@Composable
-fun getIngredientsFromDb() :List<Ingredient>{
-    val dbHandler = DBHandler(LocalContext.current);
-    return dbHandler.readIngredients()
 }
--- androidApp/src/main/java/com/example/myapplication/android/Components/forms/IngredientForm.kt
+++ androidApp/src/main/java/com/example/myapplication/android/Components/forms/IngredientForm.kt
@@ -1,35 +1,59 @@
 package com.example.myapplication.android.Components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.example.myapplication.Models.Ingredient
+import com.example.myapplication.android.SQLite.DBHandler
 
 @Composable
 fun IngredientForm(onSubmit: (Ingredient) -> Unit, showDialog: Boolean, onDismiss: () -> Unit) {
-    var name by remember { mutableStateOf("") }
-    var amount by remember { mutableStateOf("") }
+    var inputName by remember { mutableStateOf("") }
+    var amount by remember { mutableStateOf("1") }
+    var suggestedIngredient by remember { mutableStateOf("") }
+    val allIngredients = getAllIngredientsFromDb().map{it.name}
+
+    val ingredientValueChanged = {input: String ->
+        if(input.length >= 1){
+            val firstFoundIngredient = allIngredients.firstOrNull {
+                val commonPrefix = it.lowercase().commonPrefixWith(input.lowercase())
+                commonPrefix.length == input.length
+            }
+
+            if(firstFoundIngredient != null ){
+                suggestedIngredient = firstFoundIngredient
+            } else{
+                suggestedIngredient = ""
+            }
+        }
+        inputName = input
+    }
     if (showDialog) {
         Dialog(
             onDismissRequest = onDismiss,
             content = {
                 Surface(
-                    modifier = Modifier
-                        .width(280.dp)
-                        .height(240.dp),
                     elevation = 24.dp
                 ) {
                     Column(
                         modifier = Modifier.padding(16.dp)
                     ) {
                         Text(text = "Add an ingredient")
+                        if(inputName.isNotEmpty() && suggestedIngredient.isNotEmpty()){
+                            SearchItem(ItemText = suggestedIngredient, onItemClick = {inputName = suggestedIngredient})
+                        }
                         Spacer(modifier = Modifier.height(16.dp))
                         OutlinedTextField(
-                            value = name,
-                            onValueChange = { name = it },
+                            value = inputName,
+                            onValueChange = ingredientValueChanged,
                             label = { Text(text = "Name") }
                         )
                         Spacer(modifier = Modifier.height(16.dp))
@@ -40,9 +64,9 @@ fun IngredientForm(onSubmit: (Ingredient) -> Unit, showDialog: Boolean, onDismis
                         )
                         Spacer(modifier = Modifier.height(16.dp))
                         Button(
-                            onClick = { onSubmit(Ingredient(name, amount)); onDismiss()},
+                            onClick = { onSubmit(Ingredient(inputName, amount)); onDismiss()},
                             modifier = Modifier.fillMaxWidth(),
-                            enabled = name.isNotBlank() && amount.isNotBlank()
+                            enabled = inputName.isNotBlank()
                         ) {
                             Text(text = "Submit")
                         }
@@ -50,5 +74,23 @@ fun IngredientForm(onSubmit: (Ingredient) -> Unit, showDialog: Boolean, onDismis
                 }
             }
         )
+    }
+}
+
+@Composable
+fun getAllIngredientsFromDb(): List<Ingredient> {
+    val dbHandler = DBHandler(LocalContext.current);
+    return dbHandler.readIngredients()
+}
+
+@Composable
+fun SearchItem(ItemText: String, onItemClick: (String) -> Unit) {
+    Row(
+        modifier = Modifier
+            .clickable(onClick = { onItemClick(ItemText) })
+            .background(Color.LightGray)
+            .padding(PaddingValues(8.dp, 16.dp))
+    ) {
+        Text(text = ItemText, fontSize = 18.sp, color = Color.White)
     }
 }
